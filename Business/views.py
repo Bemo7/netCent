02607e770bf192b08fb0e2baa728514287c6aab1
Jaggzy7 <benjaminedet7@gmail.com>
2022-03-03 17:02:07 +0100

    final push

---
+++ Business/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render #used to render a template and return HTML markup to the client
+from django.http import HttpResponse
+
+# Create your views here.
+# a view function is a function that takes a request and returns a result. result -> response.
+# this is request handler
+# In some frameworks it's called an action but in Django it's called a view
+
+# What this function can do:
+    # Pull data from a db
+    # Transform data
+    # Send emails
+
+def say_hello(request):
+    return render(request, 'hello.html', {'name' : 'Everyone'})
+
+def restricted(request):
+    return HttpResponse('Nigga! The URL says RESTRICTED'.replace('URL','route'))
+
+def Home(request):
+    return HttpResponse('The Home page is under construction...')
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # netCent
+
+
+### Group Alpha 
