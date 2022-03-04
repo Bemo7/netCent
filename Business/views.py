f1d764913001136f71c78487f90952a32a95f531
Dammy5678 <66892442+Dammy5678@users.noreply.github.com>
2022-03-04 01:20:11 +0100

    Update views.py

--- Business/views.py
+++ Business/views.py
@@ -11,7 +11,7 @@ from django.http import HttpResponse
     # Transform data
     # Send emails
 
-def Hello(request):
+def Home(request):
     return render(request, 'hello.html', {'name' : 'Everyone'})
 
 def restricted(request):
