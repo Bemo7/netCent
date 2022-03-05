d724ba17685bec3297516ad7092099b82ba91514
Jaggzy7 <benjaminedet7@gmail.com>
2022-03-05 01:50:26 +0100

    corrections

--- Business/urls.py
+++ Business/urls.py
@@ -7,8 +7,4 @@ urlpatterns = [
     path('hello/', views.say_hello, name = "hello"),
     path('restricted/', views.restricted, name = "restricted"),
     path('', inventory_list, name="inventory_list" )
-<<<<<<< HEAD
 ]
-=======
-]
->>>>>>> 8cfda1699cb8ca5a94b7bb41b31f7812acba9f4e
