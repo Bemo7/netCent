9940c8f4aa829304fd87a697aa34c6c23bb82892
Jaggzy7 <benjaminedet7@gmail.com>
2022-03-05 01:27:07 +0100

    checking

--- Business/urls.py
+++ Business/urls.py
@@ -4,13 +4,7 @@ from . import views
 from .views import inventory_list
 # URLConf
 urlpatterns = [
-<<<<<<< HEAD
     path('hello/', views.say_hello, name = "hello"),
     path('restricted/', views.restricted, name = "restricted"),
     path('', inventory_list, name="inventory_list" )
 ]
-=======
-    path('', views.say_hello, name = "hello"),
-    path('restricted/', views.restricted, name = "restricted")
-]
->>>>>>> bef10505c224aacb477889633a7b5560abdfcb7d
